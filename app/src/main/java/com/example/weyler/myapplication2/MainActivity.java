5d36e398763e767a1d407add6f4d274a686c9826
Weyler Almeida Gomes <weyler180@gmail.com>
2017-03-15 23:51:47 -0300

    Page2

--- app/src/main/java/com/example/weyler/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/weyler/myapplication2/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.weyler.myapplication2;
 
+import android.content.Intent;
+import android.support.annotation.RequiresPermission;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    public void troca (View v) {
+        Intent page2 = new Intent(MainActivity.this,Page2.class);
+        startActivity(page2);
+    }
+
 }
