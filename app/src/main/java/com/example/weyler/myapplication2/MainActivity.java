2e5972a34dea822d6b9cbb1124701f4df1b235b3
Weyler Almeida Gomes <weyler180@gmail.com>
2017-03-16 00:38:12 -0300

    Comparing Login and Password

--- app/src/main/java/com/example/weyler/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/weyler/myapplication2/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.weyler.myapplication2;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.annotation.RequiresPermission;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,9 +19,28 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void troca (View v) {
+    public void verifica (View v) {
+        Resources res = getResources();
+        String[] logins = res.getStringArray(R.array.logins);
+        String[] senhas = res.getStringArray(R.array.senhas);
+        EditText aux = (EditText) findViewById(R.id.Login);
+        String login_atual = aux.getText().toString();
+        aux = (EditText) findViewById(R.id.Password);
+        String senha_atual = aux.getText().toString();
+
+        for (int i = 0; i < 3; i++){
+            if (login_atual.equals(logins[i])){
+                if (senha_atual.equals(senhas[i])){
+                    troca();
+                    break;
+                }
+            }
+        }
+
+    }
+
+    public void troca () {
         Intent page2 = new Intent(MainActivity.this,Page2.class);
         startActivity(page2);
     }
-
 }
